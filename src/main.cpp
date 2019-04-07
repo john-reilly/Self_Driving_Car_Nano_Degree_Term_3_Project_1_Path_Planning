db76d445456a81b2647dc575e815e1508a59a7b3
john-reilly <johnreillymct@outlook.com>
2019-04-07 16:17:25 +0000

    feat add Q+A video 2nd section complies OK

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,13 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+  
+  //inserted from Q+A video time 23.49 in video
+  //lane number
+  int lane = 1;
+  
+  double ref_vel =  49.5 ;//mph
+  
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
@@ -98,7 +105,136 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+         // from Q+A second part
           
+          
+          int prev_size = previous_path_x.size();
+          //create list of widely spaced xy points
+          vector<double>  ptsx ;
+          vector<double>  ptsy ;
+          //reference x,y,yaw states
+          double ref_x = car_x ;
+          double ref_y = car_y ;
+          double ref_yaw = deg2rad(car_yaw);
+          
+          //if previous size is almost empty use car as starting reference
+          if(prev_size <2)
+          {
+            //use 2 points that make path tangent to car
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+            
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          //else use previuos path points
+          else
+          {
+            //redefine ref state as previous path end point
+            ref_x = previous_path_x[prev_size -1 ];
+            ref_y = previous_path_y[prev_size -1 ];
+            
+            double ref_x_prev = previous_path_x[prev_size - 2] ;
+            double ref_y_prev = previous_path_y[prev_size - 2] ;
+            
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+            
+            //use 2 point to make path
+            ptsx.push_back(ref_x_prev) ;
+            ptsx.push_back(ref_x) ;
+            
+            ptsy.push_back(ref_y_prev) ;
+            ptsy.push_back(ref_y) ;
+            
+           }
+            
+            //using frenet point 
+            //tried //lane = 1; // I am getting lane not captured here going to using hardwaired number to get past it
+            int lane_number = 2 + 4 ; //as in 2 + 4 times lane;
+			 vector<double> next_wp0 = getXY(car_s + 30, lane_number, map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            
+             vector<double> next_wp1 = getXY(car_s + 60, lane_number, map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            
+             vector<double> next_wp2 = getXY(car_s + 90, lane_number, map_waypoints_s,map_waypoints_x,map_waypoints_y);
+         
+            
+          
+          
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+          
+          for(int i =0;  i< ptsx.size();i++)
+          {
+            //shift for reference
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            
+            ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+            ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+          }
+          
+          //create a spline
+          tk::spline s;
+          
+          //set xy
+          s.set_points(ptsx, ptsy);
+          //define hte aactual xy points
+         // vector<double> next_x_vals ;//redeclaration
+         // vector<double> next_y_vals ;//redeclaration
+          
+          
+          for(int i = 0; i < previous_path_x.size(); i++)
+          { 
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+            
+          }
+          
+          //calculate how to break up spline
+          double target_x = 30 ;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x)*(target_x)+(target_y)* (target_y));
+          
+          double x_add_on = 0;
+          //fill up the rest of the path planner
+          for(int i = 1; i<= 50 - previous_path_x.size();i++)
+          {//another "not captured error"
+            int ref_vel = 49.5; //the location of the first ref_vel declaration outside hte thread must be the issue??
+            double N = (target_dist/( 0.02 * ref_vel / 2.24 ) );
+            double x_point = x_add_on+(target_x)/N;
+            double y_point = s(x_point) ; 
+            
+            x_add_on = x_point ; 
+            
+            double x_ref = x_point ;
+            double y_ref = y_point ;
+            
+            //restore back after rotating earlier
+            x_point = (x_ref * cos(ref_yaw)- y_ref*sin(ref_yaw));
+            y_point = (x_ref * sin(ref_yaw)+ y_ref*cos(ref_yaw));
+            
+            x_point += ref_x;
+            y_point += ref_y;
+            
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+          
+                       
+         
+          
+          
+          
+         //from Q+A first part
+         /* 
          double dist_inc = 0.45 ;// //was 0.5; // because of 50 miles an hour
           for (int i = 0; i < 50; ++i) {
             
@@ -112,7 +248,7 @@ int main() {
               //next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));//x,y coordinates
             //next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
           } 
-          
+          */
 		// END of Student sections
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
